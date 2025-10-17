55bd00f2aeb991984c1c1c3fa81a242ab7da0a23
ramallofelipe <ramallof@udesa.edu.ar>
2025-10-17 12:34:27 -0300

    bucles terminados

--- clase09/js/index.js
+++ clase09/js/index.js
@@ -135,3 +135,55 @@ console.log(Hulk.getPoder(1))
 console.log(ironMan.getPoder(0))
 console.log(Hulk.getPoder(2))
 console.log(ironMan.getPoder(0))
+
+for (let i=0; i<5; i++) {
+    console.log('Practicando con el bucle for ');
+}
+for (let i=0; i<10; i++) {
+    console.log('La variable i tiene el valor ' + i);
+}
+let ganancias = [3213, -4829, 756, 8970, -3051, -8705, 1601, 2630, 5875,  -7301, 3041, 686]
+for (let i=0; i<ganancias.length; i++){
+    console.log(ganancias[i])
+}
+let suma = 0;
+for (let i=0; i<ganancias.length; i++){
+    suma = ganancias[i] + suma
+}
+console.log(suma)
+
+
+let healingIsDifficult = ['Fear', 'Drink to Get Drunk', 'Taken for Granted', 'Blow It All Away', 'Get Me', 'Im Not Important to You', 'Sober and Unkissed', 'Healing Is Difficult', 'Judge Me', 'Little Man', 'Insidiously']
+for (let i=0; i<healingIsDifficult.length; i++){
+    console.log(healingIsDifficult[i])
+}
+
+let got = [
+    {
+        nombre: "Jon",
+        apellido: "Snow",
+        edad: 23,
+        ciudad: "Winterfell"
+    },
+    {
+        nombre: "Daenerys",
+        apellido: "Targaryen",
+        edad: 19
+    },
+    {
+        nombre: "Arya",
+        apellido: "Stark",
+        edad: 12,
+        ciudad: "Winterfell"
+    },
+    {
+        nombre: "Tyrion",
+        apellido: "Lannister",
+        edad: 32,
+        ciudad: "Casterly Rock"
+    }
+]
+for (let i=0; i<got.length; i++){
+
+    console.log('soy ' + got[i].nombre + ' ' + got[i].apellido + ' de la ciudad ' + got[i].ciudad)
+}
